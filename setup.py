f80f8327c8666e4276acdb6e42343d29ecd0fb56
Shuheng Liu <wish1104@outlook.com>
2018-10-28 04:56:00 +0800

    style changes

--- setup.py
+++ setup.py
@@ -4,7 +4,8 @@ from setuptools import setup, find_packages
 with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
     README = readme.read()
 
-setup(name='pyAesCrypt',
+setup(
+    name='pyAesCrypt',
     version='0.4.2',
     packages = find_packages(),
     include_package_data=True,
