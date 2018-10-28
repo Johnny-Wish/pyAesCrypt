e53e6eb2d551b690d224796b7176995549f26734
Shuheng Liu <wish1104@outlook.com>
2018-10-29 04:47:34 +0800

    style changes using pep-8

--- pyAesCrypt/crypto.py
+++ pyAesCrypt/crypto.py
@@ -1,4 +1,4 @@
-#==============================================================================
+# ==============================================================================
 # Copyright 2018 Marco Bellaccini - marco.bellaccini[at!]gmail.com
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,9 +12,9 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#==============================================================================
+# ==============================================================================
 
-#==============================================================================
+# ==============================================================================
 # pyAesCrypt
 #
 # pyAesCrypt is a Python file-encryption utility that uses AES256-CBC to
@@ -32,7 +32,7 @@
 #
 # NOTE: there is no low-level memory management in Python, hence it is
 # not possible to wipe memory areas were sensitive information was stored.
-#==============================================================================
+# ==============================================================================
 
 # pyAesCrypt module
 
@@ -57,17 +57,17 @@ AESBlockSize = 16
 
 # password stretching function
 def stretch(passw, iv1):
-    
     # hash the external iv and the password 8192 times
     digest = iv1 + (16 * b"\x00")
-    
+
     for i in range(8192):
         passHash = hashes.Hash(hashes.SHA256(), backend=default_backend())
         passHash.update(digest)
         passHash.update(bytes(passw, "utf_16_le"))
         digest = passHash.finalize()
-    
+
     return digest
+
 
 # encrypt file function
 # arguments:
@@ -92,13 +92,13 @@ def encryptFile(infile, outfile, passw, bufferSize):
                 with open(outfile, "wb") as fOut:
                     # encrypt file stream
                     encryptStream(fIn, fOut, passw, bufferSize)
-                
+
             except IOError:
                 raise IOError("Unable to write output file.")
-            
+
     except IOError:
         raise IOError("File \"" + infile + "\" was not found.")
-                
+
 
 # encrypt binary stream function
 # arguments:
@@ -113,40 +113,40 @@ def encryptStream(fIn, fOut, passw, bufferSize):
     # validate bufferSize
     if bufferSize % AESBlockSize != 0:
         raise ValueError("Buffer size must be a multiple of AES block size.")
-    
+
     if len(passw) > maxPassLen:
         raise ValueError("Password is too long.")
 
     # generate external iv (used to encrypt the main iv and the
     # encryption key)
     iv1 = urandom(AESBlockSize)
-    
+
     # stretch password and iv
     key = stretch(passw, iv1)
-    
+
     # generate random main iv
     iv0 = urandom(AESBlockSize)
-    
+
     # generate random internal key
     intKey = urandom(32)
-    
+
     # instantiate AES cipher
     cipher0 = Cipher(algorithms.AES(intKey), modes.CBC(iv0),
                      backend=default_backend())
     encryptor0 = cipher0.encryptor()
-    
+
     # instantiate HMAC-SHA256 for the ciphertext
     hmac0 = hmac.HMAC(intKey, hashes.SHA256(),
                       backend=default_backend())
-    
+
     # instantiate another AES cipher
     cipher1 = Cipher(algorithms.AES(key), modes.CBC(iv1),
                      backend=default_backend())
     encryptor1 = cipher1.encryptor()
-    
+
     # encrypt main iv and key
     c_iv_key = encryptor1.update(iv0 + intKey) + encryptor1.finalize()
-    
+
     # calculate HMAC-SHA256 of the encrypted iv and key
     hmac1 = hmac.HMAC(key, hashes.SHA256(),
                       backend=default_backend())
@@ -154,52 +154,52 @@ def encryptStream(fIn, fOut, passw, bufferSize):
 
     # write header
     fOut.write(bytes("AES", "utf8"))
-    
+
     # write version (AES Crypt version 2 file format -
     # see https://www.aescrypt.com/aes_file_format.html)
     fOut.write(b"\x02")
-    
+
     # reserved byte (set to zero)
     fOut.write(b"\x00")
-    
+
     # setup "CREATED-BY" extension
     cby = "pyAesCrypt " + version
-    
+
     # write "CREATED-BY" extension length
-    fOut.write(b"\x00" + bytes([1+len("CREATED_BY"+cby)]))
-    
+    fOut.write(b"\x00" + bytes([1 + len("CREATED_BY" + cby)]))
+
     # write "CREATED-BY" extension
     fOut.write(bytes("CREATED_BY", "utf8") + b"\x00" +
                bytes(cby, "utf8"))
-    
+
     # write "container" extension length
     fOut.write(b"\x00\x80")
-    
+
     # write "container" extension
     for i in range(128):
         fOut.write(b"\x00")
-        
+
     # write end-of-extensions tag
     fOut.write(b"\x00\x00")
-    
+
     # write the iv used to encrypt the main iv and the
     # encryption key
     fOut.write(iv1)
-    
+
     # write encrypted main iv and key
     fOut.write(c_iv_key)
-    
+
     # write HMAC-SHA256 of the encrypted iv and key
     fOut.write(hmac1.finalize())
-    
+
     # encrypt file while reading it
     while True:
         # try to read bufferSize bytes
         fdata = fIn.read(bufferSize)
-        
+
         # get the real number of bytes read
         bytesRead = len(fdata)
-        
+
         # check if EOF was reached
         if bytesRead < bufferSize:
             # file size mod 16, lsb positions
@@ -211,7 +211,7 @@ def encryptStream(fIn, fOut, passw, bufferSize):
                 padLen = 0
             else:
                 padLen = 16 - bytesRead % AESBlockSize
-            fdata += bytes([padLen])*padLen
+            fdata += bytes([padLen]) * padLen
             # encrypt data
             cText = encryptor0.update(fdata) \
                     + encryptor0.finalize()
@@ -224,17 +224,18 @@ def encryptStream(fIn, fOut, passw, bufferSize):
         # ...otherwise a full bufferSize was read
         else:
             # encrypt data
-            cText = encryptor0.update(fdata)                                
+            cText = encryptor0.update(fdata)
             # update HMAC
             hmac0.update(cText)
             # write encrypted file content
             fOut.write(cText)
-    
+
     # write plaintext file size mod 16 lsb positions
     fOut.write(fs16)
-    
+
     # write HMAC-SHA256 of the encrypted file
     fOut.write(hmac0.finalize())
+
 
 # decrypt file function
 # arguments:
@@ -267,13 +268,13 @@ def decryptFile(infile, outfile, passw, bufferSize):
                         remove(outfile)
                         # re-raise exception
                         raise ValueError(str(exd))
-            
+
             except IOError:
                 raise IOError("Unable to write output file.")
-                
+
     except IOError:
         raise IOError("File \"" + infile + "\" was not found.")
-                    
+
 
 # decrypt stream function
 # arguments:
@@ -288,29 +289,29 @@ def decryptStream(fIn, fOut, passw, bufferSize, inputLength):
     # validate bufferSize
     if bufferSize % AESBlockSize != 0:
         raise ValueError("Buffer size must be a multiple of AES block size")
-    
+
     if len(passw) > maxPassLen:
         raise ValueError("Password is too long.")
 
     fdata = fIn.read(3)
     # check if file is in AES Crypt format (also min length check)
     if (fdata != bytes("AES", "utf8") or inputLength < 136):
-            raise ValueError("File is corrupted or not an AES Crypt "
-                             "(or pyAesCrypt) file.")
-        
+        raise ValueError("File is corrupted or not an AES Crypt "
+                         "(or pyAesCrypt) file.")
+
     # check if file is in AES Crypt format, version 2
     # (the only one compatible with pyAesCrypt)
     fdata = fIn.read(1)
     if len(fdata) != 1:
         raise ValueError("File is corrupted.")
-    
+
     if fdata != b"\x02":
         raise ValueError("pyAesCrypt is only compatible with version "
                          "2 of the AES Crypt file format.")
-    
+
     # skip reserved byte
     fIn.read(1)
-    
+
     # skip all the extensions
     while True:
         fdata = fIn.read(2)
@@ -319,55 +320,55 @@ def decryptStream(fIn, fOut, passw, bufferSize, inputLength):
         if fdata == b"\x00\x00":
             break
         fIn.read(int.from_bytes(fdata, byteorder="big"))
-        
+
     # read external iv
     iv1 = fIn.read(16)
     if len(iv1) != 16:
         raise ValueError("File is corrupted.")
-    
+
     # stretch password and iv
     key = stretch(passw, iv1)
-    
+
     # read encrypted main iv and key
     c_iv_key = fIn.read(48)
     if len(c_iv_key) != 48:
         raise ValueError("File is corrupted.")
-        
+
     # read HMAC-SHA256 of the encrypted iv and key
     hmac1 = fIn.read(32)
     if len(hmac1) != 32:
         raise ValueError("File is corrupted.")
-    
+
     # compute actual HMAC-SHA256 of the encrypted iv and key
     hmac1Act = hmac.HMAC(key, hashes.SHA256(),
                          backend=default_backend())
     hmac1Act.update(c_iv_key)
-    
+
     # HMAC check
     if hmac1 != hmac1Act.finalize():
         raise ValueError("Wrong password (or file is corrupted).")
-    
+
     # instantiate AES cipher
     cipher1 = Cipher(algorithms.AES(key), modes.CBC(iv1),
                      backend=default_backend())
     decryptor1 = cipher1.decryptor()
-    
+
     # decrypt main iv and key
     iv_key = decryptor1.update(c_iv_key) + decryptor1.finalize()
-    
+
     # get internal iv and key
     iv0 = iv_key[:16]
     intKey = iv_key[16:]
-    
+
     # instantiate another AES cipher
     cipher0 = Cipher(algorithms.AES(intKey), modes.CBC(iv0),
                      backend=default_backend())
     decryptor0 = cipher0.decryptor()
-    
+
     # instantiate actual HMAC-SHA256 of the ciphertext
     hmac0Act = hmac.HMAC(intKey, hashes.SHA256(),
                          backend=default_backend())
-                
+
     while fIn.tell() < inputLength - 32 - 1 - bufferSize:
         # read data
         cText = fIn.read(bufferSize)
@@ -384,10 +385,10 @@ def decryptStream(fIn, fOut, passw, bufferSize, inputLength):
         hmac0Act.update(cText)
         # decrypt data and write it to output file
         fOut.write(decryptor0.update(cText))
-        
+
     # last block reached, remove padding if needed
     # read last block
-    
+
     # this is for empty files
     if fIn.tell() != inputLength - 32 - 1:
         cText = fIn.read(AESBlockSize)
@@ -395,31 +396,31 @@ def decryptStream(fIn, fOut, passw, bufferSize, inputLength):
             raise ValueError("File is corrupted.")
     else:
         cText = bytes()
-    
+
     # update HMAC
     hmac0Act.update(cText)
-    
+
     # read plaintext file size mod 16 lsb positions
     fs16 = fIn.read(1)
     if len(fs16) != 1:
         raise ValueError("File is corrupted.")
-    
+
     # decrypt last block
     pText = decryptor0.update(cText) + decryptor0.finalize()
-    
+
     # remove padding
     toremove = ((16 - fs16[0]) % 16)
     if toremove != 0:
         pText = pText[:-toremove]
-        
+
     # write decrypted data to output file
     fOut.write(pText)
-    
+
     # read HMAC-SHA256 of the encrypted file
     hmac0 = fIn.read(32)
     if len(hmac0) != 32:
         raise ValueError("File is corrupted.")
-    
+
     # HMAC check
     if hmac0 != hmac0Act.finalize():
-        raise ValueError("Bad HMAC (file is corrupted).")   
+        raise ValueError("Bad HMAC (file is corrupted).")
--- pyAesCrypt/test_crypto.py
+++ pyAesCrypt/test_crypto.py
@@ -1,4 +1,4 @@
-#==============================================================================
+# ==============================================================================
 # Copyright 2018 Marco Bellaccini - marco.bellaccini[at!]gmail.com
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,7 +12,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#==============================================================================
+# ==============================================================================
 
 # test suite for pyAesCrypt
 
@@ -37,18 +37,18 @@ encsuffix = '.aes'
 decsuffix = '.decr'
 
 # file names
-filenames = [prefix+'empty', prefix+'block', prefix+'defbuffer',
-             prefix+'small', prefix+'med', prefix+'tbuf']
+filenames = [prefix + 'empty', prefix + 'block', prefix + 'defbuffer',
+             prefix + 'small', prefix + 'med', prefix + 'tbuf']
 
 # generate encrypted file names
 encfilenames = list()
 for f in filenames:
-    encfilenames.append(f+encsuffix)
+    encfilenames.append(f + encsuffix)
 
 # generate decrypted file names
 decfilenames = list()
 for f in filenames:
-    decfilenames.append(f+decsuffix)
+    decfilenames.append(f + decsuffix)
 
 # buffer size
 bufferSize = 64 * 1024
@@ -73,10 +73,10 @@ def genTestFiles():
         fout.write(os.urandom(4))
     # med-sized file
     with open(filenames[4], 'wb') as fout:
-        fout.write(os.urandom(2*bufferSize+19))
+        fout.write(os.urandom(2 * bufferSize + 19))
     # 3-buffer sized file
     with open(filenames[5], 'wb') as fout:
-        fout.write(os.urandom(3*bufferSize))
+        fout.write(os.urandom(3 * bufferSize))
 
 
 # function for corrupting files
@@ -112,7 +112,7 @@ class TestEncDec(unittest.TestCase):
             os.mkdir(tfdirname)
         # generate test files
         genTestFiles()
-        
+
     def tearDown(self):
         # delete test files
         for pt, ct, ou in zip(filenames, encfilenames, decfilenames):
@@ -121,7 +121,7 @@ class TestEncDec(unittest.TestCase):
             os.remove(ou)
         # delete directory for test files
         os.rmdir(tfdirname)
-    
+
     # test pyAesCrypt encryption/decryption
     def test_enc_pyAesCrypt_dec_pyAesCrypt(self):
         for pt, ct, ou in zip(filenames, encfilenames, decfilenames):
@@ -131,7 +131,7 @@ class TestEncDec(unittest.TestCase):
             pyAesCrypt.decryptFile(ct, ou, password, bufferSize)
             # check that the original file and the output file are equal
             self.assertTrue(filecmp.cmp(pt, ou))
-            
+
     # test encryption with pyAesCrypt and decryption with AES Crypt
     def test_enc_pyAesCrypt_dec_AesCrypt(self):
         for pt, ct, ou in zip(filenames, encfilenames, decfilenames):
@@ -141,7 +141,7 @@ class TestEncDec(unittest.TestCase):
             subprocess.call(["aescrypt", "-d", "-p", password, "-o", ou, ct])
             # check that the original file and the output file are equal
             self.assertTrue(filecmp.cmp(pt, ou))
-            
+
     # test encryption with AES Crypt and decryption with pyAesCrypt
     def test_enc_AesCrypt_dec_pyAesCrypt(self):
         for pt, ct, ou in zip(filenames, encfilenames, decfilenames):
@@ -151,6 +151,7 @@ class TestEncDec(unittest.TestCase):
             pyAesCrypt.decryptFile(ct, ou, password, bufferSize)
             # check that the original file and the output file are equal
             self.assertTrue(filecmp.cmp(pt, ou))
+
 
 # test binary stream functions
 class TestBS(unittest.TestCase):
@@ -166,8 +167,8 @@ class TestBS(unittest.TestCase):
             os.mkdir(tfdirname)
         # generate a test file
         with open(filenames[4], 'wb') as fout:
-            fout.write(os.urandom(2*bufferSize+19))
-        
+            fout.write(os.urandom(2 * bufferSize + 19))
+
     def tearDown(self):
         # delete test files
         os.remove(filenames[4])
@@ -175,17 +176,17 @@ class TestBS(unittest.TestCase):
         os.remove(decfilenames[4])
         # delete directory for test files
         os.rmdir(tfdirname)
-    
+
     # quick test for binary stream functions
     def test_bs_quick(self):
         # encrypt
         with open(filenames[4], "rb") as fIn:
             with open(encfilenames[4], "wb") as fOut:
                 pyAesCrypt.encryptStream(fIn, fOut, password, bufferSize)
-        
+
         # get encrypted file size
         encFileSize = os.stat(encfilenames[4]).st_size
-        
+
         # decrypt
         with open(encfilenames[4], "rb") as fIn:
             with open(decfilenames[4], "wb") as fOut:
@@ -199,13 +200,12 @@ class TestBS(unittest.TestCase):
 
 # test exceptions
 class TestExceptions(unittest.TestCase):
-    
     # test file path
     tfile = prefix + 'test.txt'
-    
+
     # path of the copy of the test file
     tfilebak = tfile + '.bak'
-    
+
     # fixture for preparing the environment
     def setUp(self):
         # make directory for test files
@@ -221,110 +221,110 @@ class TestExceptions(unittest.TestCase):
             fout.write(os.urandom(4))
         # copy of the test file
         shutil.copyfile(self.tfile, self.tfilebak)
-        
+
     def tearDown(self):
         # remove whole directory tree
         shutil.rmtree(tfdirname)
-    
+
     # test decryption with wrong password
     def test_dec_wrongpass(self):
         # encrypt file
-        pyAesCrypt.encryptFile(self.tfile, self.tfile+'.aes', password,
+        pyAesCrypt.encryptFile(self.tfile, self.tfile + '.aes', password,
                                bufferSize)
         # try to decrypt file using a wrong password
         # and check that ValueError is raised
         self.assertRaisesRegex(ValueError, ("Wrong password "
-                                                "\(or file is corrupted\)."),
+                                            "\(or file is corrupted\)."),
                                pyAesCrypt.decryptFile,
                                self.tfile + '.aes', self.tfile + '.decr',
                                'wrongpass', bufferSize)
-                               
+
         # check that decrypted file was not created
         self.assertFalse(isfile(self.tfile + '.decr'))
-            
+
     # test decryption of a non-AES-Crypt-format file
     def test_dec_not_AesCrypt_format(self):
         # encrypt file
-        pyAesCrypt.encryptFile(self.tfile, self.tfile+'.aes', password,
+        pyAesCrypt.encryptFile(self.tfile, self.tfile + '.aes', password,
                                bufferSize)
         # corrupt the 2nd byte (the 'E' of 'AES') - offset is 2-1=1
-        corruptFile(self.tfile+'.aes', 1)
-        
+        corruptFile(self.tfile + '.aes', 1)
+
         # try to decrypt file
         # ...and check that ValueError is raised
         self.assertRaisesRegex(ValueError, ("File is corrupted or "
-                                                "not an AES Crypt "
-                                                "\(or pyAesCrypt\) file."),
+                                            "not an AES Crypt "
+                                            "\(or pyAesCrypt\) file."),
                                pyAesCrypt.decryptFile,
                                self.tfile + '.aes', self.tfile + '.decr',
                                password, bufferSize)
-        
+
         # check that decrypted file was not created
         self.assertFalse(isfile(self.tfile + '.decr'))
-            
+
     # test decryption of an unsupported version of AES Crypt format
     def test_dec_unsupported_AesCrypt_format(self):
         # encrypt file
-        pyAesCrypt.encryptFile(self.tfile, self.tfile+'.aes', password,
+        pyAesCrypt.encryptFile(self.tfile, self.tfile + '.aes', password,
                                bufferSize)
         # corrupt the 4th byte
-        corruptFile(self.tfile+'.aes', 3)
-        
+        corruptFile(self.tfile + '.aes', 3)
+
         # try to decrypt file
         # ...and check that ValueError is raised
         self.assertRaisesRegex(ValueError, ("pyAesCrypt is only "
-                                                "compatible with version 2 of "
-                                                "the AES Crypt file format."),
+                                            "compatible with version 2 of "
+                                            "the AES Crypt file format."),
                                pyAesCrypt.decryptFile, self.tfile + '.aes',
                                self.tfile + '.decr', password, bufferSize)
-                               
+
         # check that decrypted file was not created
         self.assertFalse(isfile(self.tfile + '.decr'))
-            
+
     # test decryption of a file with bad hmac
     def test_dec_bad_hmac(self):
         # encrypt file
-        pyAesCrypt.encryptFile(self.tfile, self.tfile+'.aes', password,
+        pyAesCrypt.encryptFile(self.tfile, self.tfile + '.aes', password,
                                bufferSize)
-                               
+
         # get file size
-        fsize = os.stat(self.tfile+'.aes').st_size
-        
+        fsize = os.stat(self.tfile + '.aes').st_size
+
         # corrupt hmac
-        corruptFile(self.tfile+'.aes', fsize-1)
-        
+        corruptFile(self.tfile + '.aes', fsize - 1)
+
         # try to decrypt file
         # ...and check that ValueError is raised
         self.assertRaisesRegex(ValueError, ("Bad HMAC "
-                                                "\(file is corrupted\)."),
+                                            "\(file is corrupted\)."),
                                pyAesCrypt.decryptFile, self.tfile + '.aes',
                                self.tfile + '.decr', password, bufferSize)
-                               
+
         # check that decrypted file was deleted
         self.assertFalse(isfile(self.tfile + '.decr'))
-            
+
     # test decryption of a truncated file (no complete hmac)
     def test_dec_trunc_file(self):
         # encrypt file
-        pyAesCrypt.encryptFile(self.tfile, self.tfile+'.aes', password,
+        pyAesCrypt.encryptFile(self.tfile, self.tfile + '.aes', password,
                                bufferSize)
-                               
+
         # get file size
-        fsize = os.stat(self.tfile+'.aes').st_size
-        
+        fsize = os.stat(self.tfile + '.aes').st_size
+
         # truncate hmac (i.e.: truncate end of the file)
-        with open(self.tfile+'.aes', 'r+b') as ftc:
-            ftc.truncate(fsize-1)
-        
+        with open(self.tfile + '.aes', 'r+b') as ftc:
+            ftc.truncate(fsize - 1)
+
         # try to decrypt file
         # ...and check that ValueError is raised
         self.assertRaisesRegex(ValueError, "File is corrupted.",
                                pyAesCrypt.decryptFile, self.tfile + '.aes',
                                self.tfile + '.decr', password, bufferSize)
-                               
+
         # check that decrypted file was deleted
         self.assertFalse(isfile(self.tfile + '.decr'))
-    
+
     # test same input and output file - encryption
     def test_samefile_enc(self):
         self.assertRaisesRegex(ValueError, ("Input and output files "
@@ -334,6 +334,7 @@ class TestExceptions(unittest.TestCase):
                                'pass', bufferSize)
         # check that the original file was not modified
         self.assertTrue(filecmp.cmp(self.tfile, self.tfilebak))
+
     # test same input and output file - decryption
     def test_samefile_dec(self):
         self.assertRaisesRegex(ValueError, ("Input and output files "
@@ -343,6 +344,7 @@ class TestExceptions(unittest.TestCase):
                                'pass', bufferSize)
         # check that the original file was not modified
         self.assertTrue(filecmp.cmp(self.tfile, self.tfilebak))
-    
+
+
 if __name__ == '__main__':
     unittest.main()
